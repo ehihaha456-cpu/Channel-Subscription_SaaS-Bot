5411100e12bc17a96774c9f8bc41722e9d230347
ehihaha456-cpu <ehihaha456@gmail.com>
2026-07-26 15:15:01 +0530

    Add files via upload

--- handlers/clone/payment_delivery.py
+++ handlers/clone/payment_delivery.py
@@ -1,5 +1,5 @@
 """Compatibility import for clone payment delivery."""
 
-from handlers.clone.payments.delivery import ClonePaymentDeliveryMixin
+from handlers.clone.payment.delivery import ClonePaymentDeliveryMixin
 
 __all__ = ["ClonePaymentDeliveryMixin"]
